707f6de99987e84403e8c1596d3a2d3cf0123422
Wakamoly <lucidsoftworksllc@gmail.com>
2020-07-22 14:08:51 -0400

    git commit

---
+++ Pods/Target Support Files/Pods-Sabot Community-Sabot CommunityUITests/Pods-Sabot Community-Sabot CommunityUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Sabot_Community_Sabot_CommunityUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Sabot_Community_Sabot_CommunityUITests
+@end
---
+++ Pods/Target Support Files/Pods-Sabot CommunityTests/Pods-Sabot CommunityTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Sabot_CommunityTests : NSObject
+@end
+@implementation PodsDummy_Pods_Sabot_CommunityTests
+@end
