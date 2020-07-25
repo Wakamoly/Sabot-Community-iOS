a96a26c2d8b01799a41ee5804ebebf0f11f644d1
Testfake <testfake@fuckmesilly.gov>
2020-07-24 23:43:28 -0400

    git commit

---
+++ Sabot Community/SideMenuController.swift
@@ -0,0 +1,32 @@
+//
+//  SideMenuController.swift
+//  Sabot Community
+//
+//  Created by Will Murphy on 7/24/20.
+//  Copyright © 2020 LucidSoftworksLLC. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
+    
+}
