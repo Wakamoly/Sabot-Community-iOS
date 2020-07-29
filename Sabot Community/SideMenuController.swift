04d0bc2b2f80df698d16bd24b2dbc1773acd7884
Testfake <testfake@fuckmesilly.gov>
2020-07-28 23:08:45 -0400

    git commit

--- Sabot Community/SideMenuController.swift
+++ Sabot Community/SideMenuController.swift
@@ -7,30 +7,11 @@
 //
 
 import UIKit
-import Alamofire
 
 class SideMenuController: UIViewController {
     
-    @IBOutlet weak var trailCon: NSLayoutConstraint!
-    @IBOutlet weak var leadCon: NSLayoutConstraint!
-    
-    var menuOut = false
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    @IBAction func menuPress(_ sender: Any) {
-        
-        if menuOut == false {
-            leadCon.constant = 300
-            trailCon.constant = -300
-            menuOut = true
-        } else {
-            leadCon.constant = 0
-            trailCon.constant = 0
-            menuOut = false
-        }
         
     }
     
